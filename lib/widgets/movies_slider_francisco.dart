ad17c658ff80c478ede4c7aa9e60787dfb03c8a5
fmandia <fmandia1201@gmail.com>
2025-04-06 22:32:21 +0200

    page 31

--- lib/widgets/movies_slider_francisco.dart
+++ lib/widgets/movies_slider_francisco.dart
@@ -9,6 +9,57 @@ class MovieSlider extends StatelessWidget {
       width: double.infinity,
       height: 350,
       color: Colors.red,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text('Favoritas',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 20,
+                  itemBuilder: (BuildContext context, int index) =>
+                      _MoviePoster()))
+        ],
+      ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      color: Colors.green,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: const Column(
+        children: [
+          FadeInImage(
+            width: 130,
+            height: 190,
+            fit: BoxFit.cover,
+            placeholder: AssetImage(
+              'assets-francisco/no-image.jpg'
+            ),
+            image: NetworkImage(
+              'https://r-charts.com/es/miscelanea/procesamiento-imagenes-magick_files/figure-html/recortar-bordes-imagen-r.png'
+            )
+          ),
+          Text(
+            'El señor de los anillos el retorno del rey',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
